04188791010eeb8b13bd3bc9a3bb7eb25ab1ebbe
jon-zawada <jonathan_zawada@yahoo.com>
2020-11-11 10:56:51 -0800

    adds dependencies and server code to javascript template

--- templates/javascript/server/index.js
+++ templates/javascript/server/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const bodyParser = require('body-parser');
 const DIST_DIR = path.join(__dirname, '..', 'client', 'dist');
 
 app.use(express.static(DIST_DIR));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.listen(port, () => {
   console.log(`Example app listening at ${port}`)
